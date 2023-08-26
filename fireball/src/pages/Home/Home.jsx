a3112468d3ee598971524a09d3df1d6649425099
Shivangi020 <me.shivasingh.0987@gmail.com>
2023-08-27 00:30:29 +0530

    Removing navigate function

--- fireball/src/pages/Home/Home.jsx
+++ fireball/src/pages/Home/Home.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
 import {MdOutlineArrowOutward} from 'react-icons/md';
 
 
 const Home = () => {
 
-  const navigate = useNavigate();
   
-  // This function will navigate the user to explore page on clicking 'Explore now' Button 
-  const navigateExplore = ()=>{
-         navigate('/explore')
-  }
 
   return <div className="home-cn"> 
      <div className="home-tg"> <h1>Meteroite Explorer</h1></div>
     <div className="home-p"><div className="home-title"><p>How many Meteroite Landings in the 21st Century ?</p><div><p>123xx</p></div> </div>
     <p>Want to Find out MORE? </p></div>
-    <div className="cut-button"><div><a onClick={navigateExplore}>Explore now <MdOutlineArrowOutward/></a></div></div>
+    <div className="cut-button"><div><a to='/explore'>Explore now <MdOutlineArrowOutward/></a></div></div>
   </div>
 };
 
