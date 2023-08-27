32a6ea99250150d52650f16be335caabe8cec82d
Shivangi020 <me.shivasingh.0987@gmail.com>
2023-08-28 01:49:58 +0530

    Adding Responsive css for Navbar

--- fireball/src/pages/Layout/Layout.jsx
+++ fireball/src/pages/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../../Components/Navbar/Navbar";
+import Navbar from "../../Components/Navbar";
 
 const Layout = () => {
   return (
