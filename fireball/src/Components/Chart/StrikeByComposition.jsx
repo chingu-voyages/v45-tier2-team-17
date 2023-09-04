4d81abda78910d7f434876e2e313707ed8ec1436
Shivangi020 <me.shivasingh.0987@gmail.com>
2023-09-04 14:42:42 +0530

    Creating activeShape component for pie chart

--- fireball/src/Components/Chart/StrikeByComposition.jsx
+++ fireball/src/Components/Chart/StrikeByComposition.jsx
@@ -1,17 +1,87 @@
-import React from 'react'
+import React ,{useState,useCallback} from 'react'
 import { useGlobalContext } from "../../context";
-import { PieChart, Pie, Cell, Tooltip } from "recharts";
+import { PieChart, Pie,Sector} from "recharts";
+
+
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={"#E8E251"}>
+        {payload.recclass}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#fff"
+      >{`Strikes : ${value}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#fff"
+      >
+        {`(${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
+  );
+};
+
 
 
 const StrikeByComposition = () => {
 
   const { filteredData } = useGlobalContext();
   let compositionMap = {};
-
-  // Colors for Pie chart
-  const colors = [ '#d4d5d4' ,'#efc943' ,'#888888'];
-
-  const TotalCount = filteredData.length;
 
   // Calculating Number of Strikes Per Year 
   filteredData.forEach(element => {
@@ -24,45 +94,41 @@ const StrikeByComposition = () => {
 
   });
   
-
   //   Mapping the composition object into Array data structure
   const compositionData = Object.keys(compositionMap).map(recclass => ({
     recclass,
-    strikes: Number (((compositionMap[recclass] /1000 ) * 100 ).toFixed(2) )
+    strikes: compositionMap[recclass] 
   }));
 
   console.log(compositionData)
 
-  const customDataKey = (data) =>{
-    console.log(data)
-    const {strikes} = data
-      return strikes + '%' 
-  }
-  
-  
+ 
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = useCallback(
+    (_, index) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+
   return (
     <PieChart width={1000} height={500}>
-    <Pie
-      nameKey="recclass"
-      dataKey={customDataKey}
-      data={compositionData}
-      cx={500}
-      cy={200}
-      innerRadius={100}
-      outerRadius={200}
-      fill="#888888"
-      paddingAngle={0}
-    >
-        {/* {
-      compositionData.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={colors[index  > 2? index%colors.length:index ]}/>
-      ))
-    } */}
-    
-    </Pie>
-    <Tooltip />
-  </PieChart>
-  )
+      <Pie
+        activeIndex={activeIndex}
+        activeShape={renderActiveShape}
+        data={compositionData}
+        cx={500}
+        cy={200}
+        innerRadius={100}
+        outerRadius={160}
+        fill="#888888"
+        dataKey="strikes"
+        nameKey="recclass"
+        onMouseEnter={onPieEnter}
+      />
+    </PieChart>
+  );
+
 }
 
 export default StrikeByComposition
