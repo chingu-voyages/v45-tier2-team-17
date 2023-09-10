96b84ed225462d0cfa47efded12bd59e01c0db42
Rakha Randhikatama <randhikatamar@gmail.com>
2023-09-10 21:03:40 +0700

    fix: fixing piechart responsiveness.

--- fireball/src/Components/Chart/StrikeByComposition.jsx
+++ fireball/src/Components/Chart/StrikeByComposition.jsx
@@ -1,36 +1,32 @@
-import React ,{useState,useCallback} from 'react'
+import React, { useState, useCallback } from "react";
 import { useGlobalContext } from "../../context";
-import { PieChart, Pie, ResponsiveContainer} from "recharts";
-import renderActiveShape from './RenderActiveShape';
-
+import { PieChart, Pie, ResponsiveContainer } from "recharts";
+import renderActiveShape from "./RenderActiveShape";
 
 const StrikeByComposition = () => {
-
   const { filteredData } = useGlobalContext();
   let compositionMap = {};
 
-  // Calculating Number of Strikes Per Year 
-  filteredData.forEach(element => {
-    let composition = element.recclass ;
-    if(compositionMap[composition]){
-        compositionMap[composition]++;
-    }else{
-        compositionMap[composition] = 1;
-    } 
-
+  // Calculating Number of Strikes Per Year
+  filteredData.forEach((element) => {
+    let composition = element.recclass;
+    if (compositionMap[composition]) {
+      compositionMap[composition]++;
+    } else {
+      compositionMap[composition] = 1;
+    }
   });
-  
+
   // Mapping the composition object into Array data structure
-  const compositionData = Object.keys(compositionMap).map(recclass => ({
+  const compositionData = Object.keys(compositionMap).map((recclass) => ({
     recclass,
-    strikes: compositionMap[recclass] 
+    strikes: compositionMap[recclass],
   }));
- 
 
   // State activeIndex to keeps track of the currently active data point in piechart
   const [activeIndex, setActiveIndex] = useState(0);
 
-  // When a user hovers over a data point in the chart, the onPieEnter callback is called and updates the activeIndex 
+  // When a user hovers over a data point in the chart, the onPieEnter callback is called and updates the activeIndex
   // to the index of the hovered data point,
   // which is used to control the rendering of the custom active shape and tooltip within the chart
   const onPieEnter = useCallback(
@@ -41,26 +37,22 @@ const StrikeByComposition = () => {
   );
 
   return (
-    
-    <PieChart width={1000} height={500}>
-      <Pie
-        activeIndex={activeIndex}
-        activeShape={renderActiveShape}
-        data={compositionData}
-        cx={500}
-        cy={200}
-        innerRadius={100}
-        outerRadius={160}
-        fill="#888888"
-        dataKey="strikes"
-        nameKey="recclass"
-        onMouseEnter={onPieEnter}
-      />
-    </PieChart>
-   
-
+    <ResponsiveContainer width="100%" height={495}>
+      <PieChart>
+        <Pie
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape}
+          data={compositionData}
+          innerRadius={100}
+          outerRadius={160}
+          fill="#888888"
+          dataKey="strikes"
+          nameKey="recclass"
+          onMouseEnter={onPieEnter}
+        />
+      </PieChart>
+    </ResponsiveContainer>
   );
+};
 
-}
-
-export default StrikeByComposition
+export default StrikeByComposition;
