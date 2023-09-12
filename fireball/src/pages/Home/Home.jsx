e1401330cf4ba8e6d00a91976d5f54809052b2e5
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-09-12 21:08:05 +0900

    updated home page mobile css with mediaqueries

--- fireball/src/pages/Home/Home.jsx
+++ fireball/src/pages/Home/Home.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
 
-  return <div className="home-cn"> 
+  return <div className="home-cn">
      <div className="home-tg"> <h1>Meteroite Explorer</h1></div>
-    <div className="home-p"><div className="home-title"><p>How many Meteroite Landings in the 21st Century ?</p><div><p>123xx</p></div> </div>
+    <div className="home-p"><div className="home-title"><p>How many Meteroite Landings in the 21st Century ?</p><div className = "number"><p>123xx</p></div></div>
     <p>Want to Find out MORE? </p></div>
     <div className="cut-button"><Link to='/explore'>Explore now <MdOutlineArrowOutward/></Link></div>
   </div>
