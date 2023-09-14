ac7a62109679703e63dd31ffaa8ff968290fccfb
Paulette <paulettz@icloud.com>
2023-09-14 20:43:20 +0900

    Mobile styling FIR-18 (#35)
    * Update README.md (#25)
    
    * Update README.md
    
    * Update README.md
    
    * updated home page mobile css with mediaqueries
    
    * Update README.md
    
    * updated pagination buttons, and footer size for mobile

--- fireball/src/pages/Home/Home.jsx
+++ fireball/src/pages/Home/Home.jsx
@@ -7,9 +7,11 @@ const Home = () => {
 const {data} = useGlobalContext()
 const totalStrikes = data.length;
 
-  return <div className="home-cn"> 
+  return <div className="home-cn">
      <div className="home-tg"> <h1>Meteroite Explorer</h1></div>
+
     <div className="home-p"><div className="home-title"><p>How many Meteroite Landings in the 21st Century ?</p><div><p>{totalStrikes}</p></div> </div>
+
     <p>Want to Find out MORE? </p></div>
     <div className="cut-button"><Link to='/explore'>Explore now <MdOutlineArrowOutward/></Link></div>
   </div>
